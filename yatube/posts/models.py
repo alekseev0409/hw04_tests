3b73303c126fe711641d669c7c0690f41382b0ee
Алексей Алексеев <alekseev0409@gmail.com>
2023-03-03 20:18:19 +0300

    finish

---
+++ yatube/posts/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(verbose_name='Заголовок', max_length=200)
+    slug = models.SlugField(verbose_name='ссылка', unique=True)
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(verbose_name='Текст поста')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    group = models.ForeignKey(
+        Group,
+        verbose_name='Группа',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts')
+
+    class Meta:
+        ordering = ['pub_date']
+
+    def __str__(self):
+        return self.text[:15]
